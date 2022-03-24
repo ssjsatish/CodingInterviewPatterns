cdf7fdbe12603f50e57eda7ccc88a688fef05a5d
Satish Patel <Satish.Patel@icfnext.com>
2022-03-24 16:23:43 +0530

    Starting sliding window pattern

---
+++ coding-interview-patterns/src/main/java/slidingwindow/PairWithTarget.java
@@ -0,0 +1,10 @@
+package slidingwindow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PairWithTarget {
+    public List<Integer> findPair(List<Integer> arr, int targetSum) {
+        return new ArrayList<Integer>();
+    }
+}
